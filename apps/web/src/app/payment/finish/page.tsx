b2963aac0bf3bbc027d9053a0b96acafbfbdf59a
carrFX <164209128+carrFX@users.noreply.github.com>
2024-11-18 23:29:05 +0700

    feat: add favico and run build

--- apps/web/src/app/payment/finish/page.tsx
+++ apps/web/src/app/payment/finish/page.tsx
@@ -3,7 +3,7 @@ const page = () => {
   return (
     <section className="min-h-screen bg-beigeCustom w-full flex flex-col justify-center items-center">
       <IoMdTime className="w-24 h-24 md:w-36 md:h-36 text-black" />
-      <h1 className="text-3xl md:text-6xl font-semibold uppercase tracking-wide text-black mt-2 mb-0 md:mb-3 md:mt-5">Payment Success</h1>
+      <h1 className="text-3xl md:text-6xl font-semibold uppercase tracking-wide text-black mt-2 mb-0 md:mb-3 md:mt-5">Your payment is being processed</h1>
     </section>
   )
 }
